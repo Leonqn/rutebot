3cb5fafcba33f37f1166de261a98441e3d7d6f87
Ilya Titkov <ilya@titkov.me>
2019-04-18 22:07:03 +0500

    add into<chatId>

--- tests/basic_req_res_tests.rs
+++ tests/basic_req_res_tests.rs
@@ -1,7 +1,7 @@
 use pretty_assertions::{assert_eq, assert_ne};
 
 use rutebot::requests::get_me::GetMe;
-use rutebot::responses::User;
+use rutebot::responses::{User, Message};
 
 use crate::common::run_one;
 use rutebot::requests::send_message::send_text_message::SendTextMessageRequest;
@@ -17,3 +17,12 @@ pub fn get_me_works() {
     assert_eq!(response.is_bot, true);
 }
 
+#[test]
+pub fn send_text_message_works() {
+    let rutebot = common::create_client();
+    let chat_id = common::get_chat_id();
+
+    let response: Message = run_one(rutebot.prepare_api_request(&SendTextMessageRequest::new(chat_id, "Some text")).send());
+
+    assert_eq!(response.text.unwrap(), "Some text");
+}
