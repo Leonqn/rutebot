1a459a4c6fff9aa8fe391da1a04e30929cde6949
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-04-29 17:15:01 +0200

    Extract handle_update from update loop in echobot example

--- examples/echobot.rs
+++ examples/echobot.rs
@@ -2,8 +2,39 @@ use hyper::rt::{Future, Stream};
 use std::env;
 
 use rutebot::client::Rutebot;
-use rutebot::requests::{GetUpdates, SendMessage};
+use rutebot::requests::{GetUpdates, Request, SendMessage};
 use rutebot::responses::{Message, Update};
+
+fn handle_update(update: Update) -> Option<impl Request> {
+    match update {
+        Update {
+            message:
+                Some(Message {
+                    message_id,
+                    ref chat,
+                    text: Some(ref text),
+                    ..
+                }),
+            ..
+        } => {
+            let request = SendMessage::new_reply(chat.id, text, message_id);
+            Some(request)
+        }
+        Update {
+            message:
+                Some(Message {
+                    message_id,
+                    ref chat,
+                    ..
+                }),
+            ..
+        } => {
+            let request = SendMessage::new_reply(chat.id, "This is not text...", message_id);
+            Some(request)
+        }
+        _ => None,
+    }
+}
 
 fn main() {
     let token_env = env::var_os("TELEGRAM_TOKEN")
@@ -18,47 +49,21 @@ fn main() {
     let updates = rutebot
         .incoming_updates(get_updates)
         .then(Ok)
-        .for_each(move |x| {
-            let reply_msg_request = match x {
-                Ok(Update {
-                    message:
-                        Some(Message {
-                            message_id,
-                            ref chat,
-                            text: Some(ref text),
-                            ..
-                        }),
-                    ..
-                }) => {
-                    let request = SendMessage::new_reply(chat.id, text, message_id);
-                    Some(request)
-                }
-                Ok(Update {
-                    message:
-                        Some(Message {
-                            message_id,
-                            ref chat,
-                            ..
-                        }),
-                    ..
-                }) => {
-                    let request =
-                        SendMessage::new_reply(chat.id, "This is not text...", message_id);
-                    Some(request)
+        .for_each(move |update_result| {
+            match update_result {
+                Ok(update) => {
+                    if let Some(request) = handle_update(update) {
+                        let send_future = rutebot
+                            .prepare_api_request(request)
+                            .send()
+                            .map(|_| ())
+                            .map_err(|x| println!("Got error while sending message: {:?}", x));
+                        hyper::rt::spawn(send_future);
+                    };
                 }
                 Err(e) => {
                     println!("Got error while getting updates {:?}", e);
-                    None
                 }
-                _ => None,
-            };
-            if let Some(reply) = reply_msg_request {
-                let send_future = rutebot
-                    .prepare_api_request(reply)
-                    .send()
-                    .map(|_| ())
-                    .map_err(|x| println!("Got error while sending message: {:?}", x));
-                hyper::rt::spawn(send_future);
             }
             Ok(())
         });
